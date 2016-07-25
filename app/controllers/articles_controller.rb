2f20929f42babb53290856be1b5e4a4a39d35b23
Newton Xu <newton.xu@hotmail.com>
2016-07-25 03:52:23 +0000

    Forms added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,37 @@
 class ArticlesController < ApplicationController
+    include ArticleHelper
     def index
         @articles = Article.all
     end
     def show
         @articles = Article.find(params[:id]) 
     end
+    def new 
+        @articles = Article.new
+    end 
+    def create 
+        @articles = Article.new(article_params)
+        @articles.save
+        flash.notice = "Article '#{@articles.title}' created!"
+        redirect_to article_path(@articles)
+    end
+    
+    def destroy 
+        Article.find(params[:id]).destroy
+        
+        flash.notice = "Article deleted!"
+        redirect_to action: 'index', status: 303
+    end
+    
+    def edit
+        @articles = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
+    end 
 end
